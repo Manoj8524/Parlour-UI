dacefd460180277f397b1a8e7987036894c59c4e
Manoj8524 <164409823+Manoj8524@users.noreply.github.com>
2024-08-30 14:20:06 +0900

    Add files via upload

---
+++ jerin-parlour-client/src/Components/Home/Testimonials/Testimonials.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Testimonial from './Testimonial';
+
+const Testimonials = () => {
+    return (
+        <div className='mid-container'>
+            <h1 className='text-center text-4xl font-bold lg:mb-20 md:mb-16 mb-16'>Testimonials</h1>
+            <div>
+                <Testimonial />
+            </div>
+        </div>
+    );
+};
+
+export default Testimonials;
